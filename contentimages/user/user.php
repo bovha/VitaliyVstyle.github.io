216ae5cefc0d84e8d822c0fca611fe60c3081913
Vitaliy V <serials101@gmail.com>
2017-06-18 09:59:36 +0300

    .

--- contentimages/user/user.php
+++ contentimages/user/user.php
@@ -2,18 +2,18 @@
 $str=getenv("QUERY_STRING");
 if ($str) {
     $str=urldecode($str);
-    $str=str_replace("...", "</td><td>", $str);
-} else $str="---</td><td>---</td><td>---";
+    $str=str_replace("...", "<br>", $str);
+} else $str="N/a<br>N/a<br>N/a";
 $ip=getenv("REMOTE_ADDR");
 date_default_timezone_set("Europe/Moscow");
 $datetime=date("d/F/y H.i:s", time());
 $user=getenv("HTTP_USER_AGENT");
 if (!$user)
-    $user="---";
+    $user="N/a";
 $locale=getenv("HTTP_ACCEPT_LANGUAGE");
 $page=getenv("HTTP_REFERER");
 if (!$page)
-    $page="---";
+    $page="N/a";
 $file="visitors.txt";
 $filesiz=filesize($file);
 if (!file_exists($file) || $filesiz/1024 > 3072)
@@ -24,7 +24,7 @@ if (flock($fp, LOCK_EX)) {
 rewind($fp);
 $old=fread($fp, $filesiz);
 rewind($fp);
-fwrite($fp, "<tr><td>$page</td><td>$str</td><td>$user</td><td>$ip</td><td>$locale</td><td>$datetime</td></tr>\r\n$old");
+fwrite($fp, "<tr><td>$datetime | $ip<br>$page | $locale<br>$user</td><td>$str</td></tr>\r\n$old");
 flock($fp, LOCK_UN);
 }
 fclose($fp);
