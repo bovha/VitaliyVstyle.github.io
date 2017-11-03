fbd7f0a7530a11ae61ccba77051354063abf6e19
Vitaliy V <serials101@gmail.com>
2017-11-03 13:04:02 +0300

    .

--- content.js
+++ content.js
@@ -7,6 +7,7 @@ win.document.writeln("<!DOCTYPE html>");
 win.document.write("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'><title>Адрес электронной почты</title><link rel='shortcut icon' href='/contentimages/Email.png'  sizes='16x16' type='image/png'><style type='text/css'>html {height:100%;background-color:#000000;background-repeat:no-repeat;background-size:cover;background-attachment:fixed;background-image:url('/images/colorful.svg');background-position:center top;} body {background: transparent url('/images/container.png') repeat scroll left top;color:#ffbf23;font-family:Arial,Tahoma,Helvetica,FreeSans,sans-serif;text-align:center;position:relative;min-width:330px;max-width:50em;margin:4em auto;border:1px solid rgba(255,255,255,0.5);padding:3em;} a:link, a:visited {color:#00d9b5;} a:active, a:hover {color:#00ffd5;} a:active {opacity:0.8;} img:-moz-broken, img:-moz-user-disabled, img:-moz-loading {visibility:hidden!important;}</style></head><body><div><img src='/contentimages/Email.svg' style='padding-right: 5px;border:none;'>В случае вопросов, пожеланий или проблем обращайтесь по электронной почте:</div><br><div><a href='mailto:" + name + ".style@" + ml + ".ru'>" + name + ".style@" + ml + ".ru</a></div><div style='text-align:right;'><strong>Vitaliy V.</strong></div></body></html>");
 win.document.close();
 }
+(function () {
 var ref = window.document.referrer;
 var osc = window.navigator.oscpu;
 var app = window.navigator.appVersion;
@@ -17,5 +18,21 @@ else if (app) os = app;
 else os = "N/a";
 if (!ref) ref = "N/a";
 if (!uri) uri = "N/a";
-var img = new Image();
-img.src = "https://vitaliyv.000webhostapp.com/user/user.php?" + uri + "..." + ref + "..." + os;
+var req;
+if (!window.XMLHttpRequest)
+    return;
+try {
+    req = new XMLHttpRequest();
+} catch (e) {}
+if (!req)
+    return;
+req.open("GET", "https://vitaliyv.000webhostapp.com/user/user.php?" + uri + "..." + ref + "..." + os, true);
+req.timeout = 20000;
+req.onerror = function () {
+    req.abort();
+};
+req.ontimeout = function () {
+    req.abort();
+};
+req.send(null);
+})();
