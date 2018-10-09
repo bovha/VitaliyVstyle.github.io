6dd9569f7fcb72ef0dbb2816b0e272cfac6368ac
VitaliyV <serials101@gmail.com>
2018-10-09 12:01:42 +0300

    .

--- files/linux/copy_move/rename.sh
+++ files/linux/copy_move/rename.sh
@@ -4,17 +4,17 @@ regExp="s/[^0-9](?=.*\.)//g|s/[0-9]+/substr($&,0,3)/e|s/[0-9]+/sprintf('%03d',$&
 selAll=false
 spAll=false
 for (( i=1; i<=nArgs; i++ )); do
-    fn=${1##*/}
-    rename  -n -d "$regExp" "${1}"
+    fn=${1}
+    rename  -n -d "$regExp" "$fn"
     if [ false = "$selAll" ]; then
         sel=$(zenity --list --radiolist --title="Переименовать файл?" --column="" --column="" --hide-header --width=500 --height=170 --cancel-label="Пропустить" --text="Выберите действие из списка ниже" TRUE "Переименовать $fn" FALSE "Переименовать все" FALSE "Пропустить все")
         case "$sel" in
             "Переименовать $fn" )
-                rename -d "$regExp" "${1}"
+                rename -d "$regExp" "$fn"
             ;;
             "Переименовать все" )
                 selAll=true
-                rename -d "$regExp" "${1}"
+                rename -d "$regExp" "$fn"
             ;;
             "Пропустить все" )
                 selAll=true
@@ -28,7 +28,7 @@ for (( i=1; i<=nArgs; i++ )); do
     elif [ true = "$spAll" ]; then
         echo "Пропуск: $fn"
     else
-        rename -d "$regExp" "${1}"
+        rename -d "$regExp" "$fn"
     fi
     shift 1
 done
