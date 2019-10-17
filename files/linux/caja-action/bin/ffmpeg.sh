1b55a0eea0858bb41b833d899c5b70cdfb1142d0
VitaliyVstyle <serials101@gmail.com>
2019-10-17 13:13:29 +0300

    2019.10.17 13:13:27

--- files/linux/caja-action/bin/ffmpeg.sh
+++ files/linux/caja-action/bin/ffmpeg.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 if [ -n "$1" ]; then
+    SCRIPT_DIR=$(dirname "$0")
+    DIR=""
+    read -r DIR < "$SCRIPT_DIR/directory.txt"
+    if [ -z "$DIR" ]; then
+        DIR="$HOME"
+        echo "$DIR" > "$SCRIPT_DIR/directory.txt"
+    fi
+    cd "$DIR" || {
+        DIR="$HOME"
+        cd "$DIR" || exit 1
+    }
     DIR=$(zenity --file-selection --directory --title="Выбрать директорию")
     if [ "$?" -eq 1 ]; then
         exit 1
     fi
+    echo "$DIR" > "$SCRIPT_DIR/directory.txt"
+    cd "$DIR" || exit 1
     NAME=$(zenity --entry --title="Название файла" --text="Введите название файла (без расширения):" --entry-text="mp4video")
     if [ "$?" -eq 1 ]; then
         exit 1
@@ -13,5 +26,5 @@ if [ -n "$1" ]; then
     if [ "$NAME" = "" ]; then
         NAME="mp4video"
     fi
-    mate-terminal --profile=youtube-dl --command "ffmpeg -i '$1' -c:a copy -c:v copy -f mp4 '$DIR'/'$NAME'.mp4"
+    mate-terminal --profile=youtube-dl --command "ffmpeg -i $1 -c:a copy -c:v copy -f mp4 $NAME.mp4"
 fi
--- files/linux/caja-action/bin/youtube_dl.sh
+++ files/linux/caja-action/bin/youtube_dl.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 if [ -n "$1" ]; then
+    SCRIPT_DIR=$(dirname "$0")
+    DIR=""
+    read -r DIR < "$SCRIPT_DIR/directory.txt"
+    if [ -z "$DIR" ]; then
+        DIR="$HOME"
+        echo "$DIR" > "$SCRIPT_DIR/directory.txt"
+    fi
+    cd "$DIR" || {
+        DIR="$HOME"
+        cd "$DIR" || exit 1
+    }
     DIR=$(zenity --file-selection --directory --title="Выбрать директорию")
     if [ "$?" -eq 1 ]; then
         exit 1
     fi
-    mate-terminal --profile=youtube-dl --working-directory="$DIR/" --command "$1"
+    echo "$DIR" > "$SCRIPT_DIR/directory.txt"
+    cd "$DIR" || exit 1
+    mate-terminal --profile=youtube-dl --command "$1"
 fi
 
 if [ -n "$2" ]; then
